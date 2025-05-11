25d69d069fc3f715cd009d3093ab376e2a321563
DanaHaham <danahak@wix.com>
2025-05-12 01:42:50 +0300

    stop

--- code/src/main.py
+++ code/src/main.py
@@ -14,8 +14,14 @@ def main():
         forwarder.start()
     except KeyboardInterrupt:
         logging.info("DNS Forwarder stopped by user")
+        forwarder.stop()  # Ensure we stop the forwarder when interrupted
     except Exception as e:
         logging.error("DNS Forwarder stopped due to error: %s", str(e))
+        forwarder.stop()  # Also stop the forwarder on any other error
+    finally:
+        # Make sure we always stop the forwarder, even if an unexpected error occurs
+        if forwarder:
+            forwarder.stop()
 
 if __name__ == "__main__":
     main() 
